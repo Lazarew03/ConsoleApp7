42f1c9812c0a11142f943897cecb650d38a65906
bogor <bogor@DESKTOP-DC6RO5J>
2022-01-30 18:47:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+        var arr = new[,]
+        {
+{1, 0, 0},
+{0, 0, 0},
+{0, 8, 0}
+};
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[i, j] == 0)
+                    arr[i, j] = 1;
+            }
+        }
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
